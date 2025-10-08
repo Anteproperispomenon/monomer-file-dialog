d9e02d6abdb54aca5dd47673fa8185a8ec9c2b69
David Wilson <30732161+Anteproperispomenon@users.noreply.github.com>
2025-10-07 23:00:10 -0400

    Added code for showing file sizes.

--- src/Monomer/Widgets/Extra/FileDialog/Internal.hs
+++ src/Monomer/Widgets/Extra/FileDialog/Internal.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE PackageImports #-}
+
 module Monomer.Widgets.Extra.FileDialog.Internal
   ( FileData(..)
   , PathKind(..)
@@ -12,11 +14,19 @@ import Control.Monad
 
 import System.IO.Error qualified as IOE
 
+import Data.Text qualified as T
+import TextShow (showt, showb, toText)
+
+import Numeric
+
+import TextShow.Data.Floating
+
 data FileData = FileData
   { fdName :: OsPath
   , fdPath :: OsPath -- Full, absolutePath.
   , fdExtn :: Maybe OsString
   , fdKind :: PathKind
+  , fdSize :: Maybe Integer
   } deriving (Show, Eq)
 
 data PathKind
@@ -31,8 +41,8 @@ getDirData pth = do
   pths <- map (pth </>) <$> listDirectory pth
   dirs <- filterM doesDirectoryExist pths
   fils <- filterM doesFileExist pths
-  let dirs2 = map (\fp -> FileData (takeFileName fp) fp Nothing           PathDir ) dirs
-      fils2 = map (\fp -> FileData (takeFileName fp) fp (getExtnMaybe fp) PathFile) fils
+  let dirs2 = map  (\fp -> FileData (takeFileName fp) fp Nothing           PathDir      Nothing        ) dirs
+  fils2    <- mapM (\fp -> FileData (takeFileName fp) fp (getExtnMaybe fp) PathFile <$> getFileSize' fp) fils
   return (dirs2, fils2)
   where
     getExtnMaybe :: OsPath -> Maybe OsString
@@ -40,12 +50,26 @@ getDirData pth = do
       | (hasExtension pt) = Nothing
       | otherwise         = Just (takeExtensions pt)
 
+getFileSize' :: OsPath -> IO (Maybe Integer)
+getFileSize' osPath = Just <$> getFileSize osPath
 
-
-
-
-
-
-
-
-
+showSize :: Integer -> T.Text
+showSize n
+  | n < 1024   = toText (showb n <> " B")
+  | newM ==  1 = toText $ fltNum <> " KiB"
+  | newM ==  2 = toText $ fltNum <> " MiB"
+  | newM ==  3 = toText $ fltNum <> " GiB"
+  | newM ==  4 = toText $ fltNum <> " TiB"
+  | newM ==  5 = toText $ fltNum <> " PiB"
+  | newM ==  6 = toText $ fltNum <> " EiB"
+  | newM ==  7 = toText $ fltNum <> " ZiB"
+  | newM ==  8 = toText $ fltNum <> " YiB"
+  | newM ==  9 = toText $ fltNum <> " RiB"
+  | newM == 10 = toText $ fltNum <> " QiB"
+  | otherwise  = toText $ (showbEFloat (Just 2) nFlo) <> " B"
+  where
+    nFlo :: Double
+    nFlo = fromInteger n
+    mags = logBase 1024 nFlo
+    newM = floor mags
+    fltNum = showbFFloat (Just 2) (nFlo / (1024 ^^ newM))
